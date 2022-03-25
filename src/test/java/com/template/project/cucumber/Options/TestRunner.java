3bb6cba86ade5e2ef7f7bf281f71389aca4cde00
rajaspar <parthiban.rajasekaran@diconium.com>
2022-03-25 21:16:40 +0100

    add multiple html cucumber report generation

--- src/test/java/com/template/project/cucumber/Options/TestRunner.java
+++ src/test/java/com/template/project/cucumber/Options/TestRunner.java
@@ -5,10 +5,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/com/template/project/features", plugin = {"json:target/jsonReports/cucumber-report.json", "html:test-output", "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},monochrome = true , glue = {
+@CucumberOptions(features = "src/test/java/com/template/project/features", plugin = { "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "html:target/cucumber-report.html", "json:target/jsonReports/cucumber-report.json"}, monochrome = true , glue = {
     "com/template/project/stepDefinitions"}, publish = true,tags = "@Customer and not @Creation")
 public class TestRunner {
-
 }
 
 // mvn clean test -D"cucumber.filter.tags=@Customer and not @Creation"
+
+// plugin options for reporting : "json:target/jsonReports/cucumber-report.json", "html:target/cucumber-report", "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
